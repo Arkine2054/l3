073d15e72ed02aae9d5aa3b0b34f3ff65d9293a9
Nikita <nikita2054@gmail.com>
2025-11-26 18:15:58 +0300

    l3.1

--- 1/test_email.go
+++ 1/test_email.go
@@ -15,7 +15,6 @@ func main() {
 		"Subject: Test Email\r\n\r\n" +
 		"This is a test email from worker.\r\n")
 
-	// Отправляем письмо
 	if err := smtp.SendMail(smtpAddr, nil, from, []string{to}, msg); err != nil {
 		log.Fatalf("failed to send email: %v", err)
 	}
